43d3a0b33046ee36843190a51831eba02fb73790
NoName <foobar@example.com>
2025-01-17 19:49:40 +0300

    1st bot

---
+++ loader.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher,Router
+from config.token import TOKEN
+import sqlite3
+con = sqlite3.connect('data.db')
+cursor = con.cursor()
+
+router = Router()
+dp = Dispatcher()
+dp.include_router(router)
+bot= Bot(TOKEN)
